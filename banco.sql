68ecdc4b829de7b9f21d92093771e84f480e6df7
Ricardo Barreto Souza Santos <ricbarreto27@gmail.com>
2025-03-20 22:21:24 -0300

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -1,8 +1,8 @@
-CREATE TABLE dados (
-    AlunoID int,
-    Nome varchar(50),
-    Sobrenome varchar(50),
-    Endereco varchar(150),
-    Cidade varchar(50),
-    Host varchar(50)
+CREATE TABLE alunos (
+    aluno_id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50) NOT NULL,
+    sobrenome VARCHAR(50) NOT NULL,
+    endereco VARCHAR(100),
+    cidade VARCHAR(50),
+    host VARCHAR(50)
 );
